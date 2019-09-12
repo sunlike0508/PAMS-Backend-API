31bf4c479ecc4adb5820d7923671f934d663546e
JinHoooooou <jinho4744@naver.com>
2019-09-12 11:55:22 +0900

    PAMS-BE-19-회원가입 기능 구현

---
+++ src/main/java/com/pams/user/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.pams.user.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pams.common.protocol.CommonResponseVO;
+import com.pams.common.protocol.CommonResultCode;
+import com.pams.user.dto.User;
+import com.pams.user.service.UserService;
+
+@RestController
+@RequestMapping("/pams")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	//Create
+	@RequestMapping(value = "/signUpUser", method = RequestMethod.POST)
+    public @ResponseBody CommonResponseVO signUpUser(@RequestBody User user) {
+		
+		CommonResponseVO response = userService.signUpUser(user);
+	
+        return response;
+    }
+	
+	//Read
+	
+	//Update
+	
+	//Delete
+}
---
+++ src/main/java/com/pams/user/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.pams.user.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.pams.common.protocol.CommonResponseVO;
+import com.pams.common.protocol.CommonResultCode;
+import com.pams.user.dto.User;
+import com.pams.user.repo.UserRepository;
+
+import lombok.extern.java.Log;
+
+@Log
+@JsonSerialize
+@Service("userService")
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public CommonResponseVO signUpUser(User user) {
+		
+		//need validation check
+		User signUpUser = userRepo.save(user);
+		
+		CommonResponseVO response = new CommonResponseVO();
+		response.setResponseCode(CommonResultCode.SUCCESS_NORMAL.getCode());
+		response.setResponseMessage(CommonResultCode.SUCCESS_NORMAL.getMessage());
+		response.setResponseData(signUpUser);
+		
+		return response;
+	}
+
+}
