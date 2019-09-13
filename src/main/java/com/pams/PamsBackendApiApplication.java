4953d53bf267d9cd63f0579aebd9e23dd8ee27e7
JinHoooooou <jinho4744@naver.com>
2019-09-13 15:32:23 +0900

    PAMS-BE-19 DB암호화를 위한 main class에어노테이션추가

--- src/main/java/com/pams/PamsBackendApiApplication.java
+++ src/main/java/com/pams/PamsBackendApiApplication.java
@@ -3,6 +3,9 @@ package com.pams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@EnableEncryptableProperties
 @SpringBootApplication
 public class PamsBackendApiApplication {
 
