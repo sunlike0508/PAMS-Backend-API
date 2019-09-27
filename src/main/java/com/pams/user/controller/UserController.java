12aa506bcc0d12d503a36801766a7205c40c2b7a
JinHoooooou <jinho4744@naver.com>
2019-09-27 13:43:35 +0900

    GET방식에 맞게 getUserList메소드 수정

--- src/main/java/com/pams/user/controller/UserController.java
+++ src/main/java/com/pams/user/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.pams.user.controller;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pams.common.protocol.CommonResponseVO;
-import com.pams.common.protocol.CommonResultCode;
 import com.pams.user.dto.User;
 import com.pams.user.service.UserService;
 
@@ -30,7 +30,7 @@ public class UserController {
     }
 	
 	@RequestMapping(value ="/getUserList", method = RequestMethod.GET)
-	public @ResponseBody CommonResponseVO getUserList(@RequestBody User user) {
+	public @ResponseBody CommonResponseVO getUserList(@ModelAttribute User user) {
 		
 		CommonResponseVO response = userService.getUserList(user);
 		
